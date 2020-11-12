86715e1dc5204aa81cc9f84ddff5601d60c053ce
Sam Evans <evansst@icloud.com>
2020-11-12 10:36:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-'\s]/gi, '');
+  }
+
+  static titleize(string) {
+    const ignored = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'and', 'from'];
+    return this.capitalize(string)
+      .split(' ')
+      .map((word) => (ignored.includes(word) ? word : this.capitalize(word)))
+      .join(' ');
+  }
 }
